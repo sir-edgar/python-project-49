6f003693afbc20f27022dbb3921be6a72ffe3b61
Timur Vitrov <edgar.sir@yandex.ru>
2024-06-26 17:05:57 +0300

    changed brain-prime to correct format

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,44 +1,11 @@
-import random
-import prompt
-
-
-def is_prime():
-    global number
-    global answer
-    number = random.randint(1, 100)
-    print(f'Question: {number}')
-    if number <= 1:
-        answer = 'no'
-        return False
-    for i in range(2, int(number ** 0.5) + 1):
-        if number % i == 0:
-            answer = 'no'
-            return False
-    answer = 'yes'
-    return True
-
-
-def prime_game():
-    print("Welcome to the Brain Games!")
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    print("Answer 'yes' if given number is prime. Otherwise answer 'no'.")
-    points = 0
-    THREE_POINTS = 3
-    while points < THREE_POINTS:
-        is_prime()
-        users_answer = input('Your answer: ')
-        if users_answer == answer:
-            points += 1
-            print('Correct!')
-        else:
-            print(f"'{users_answer}' is wrong answer ;(. Correct answer was '{answer}'. \nLet's try again, {name}!")
-            return
-    print(f'Congratulations, {name}!')
+#!/usr/bin/env python3
+from brain_games.games import prime
+from brain_games.engine import universal_engine
 
 
 def main():
-    prime_game()
+    universal_engine(prime)
+
 
 if __name__ == '__main__':
     main()
