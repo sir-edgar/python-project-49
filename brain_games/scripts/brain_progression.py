9630a8f5cbd87356925ba678e504e69490f7dab4
Timur Vitrov <edgar.sir@yandex.ru>
2024-06-26 17:10:31 +0300

    changed brain-progression to correct format

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,48 +1,11 @@
-import random
-import prompt
-
-
-
-def progression():
-    i = 0
-    a = random.randint(1, 50)
-    b = random.randint(1, 5)
-    int_fot_points = random.randint(1, 10)
-    result = str(a) + ' '
-    while i < 10:
-        c = (a + b)
-        a = c
-        i += 1
-        if i != int_fot_points:
-            result += str(c) + ' '
-        else:
-            global answer
-            answer = c
-            result += '.. '
-    print(f'Question: {result}')
-
-
-def progression_game():
-    print("Welcome to the Brain Games!")
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    print("What number is missing in the progression?")
-    points = 0
-    THREE_POINTS = 3
-    while points < THREE_POINTS:
-        progression()
-        users_answer = input('Your answer: ')
-        if str(answer) == users_answer:
-            print(f'Correct!')
-            points += 1
-        else:
-            print(f"'{users_answer}' is wrong answer ;(. Correct answer was '{answer}'. \nLet's try again, {name}!")
-            return
-    print(f'Congratulations, {name}!')
+#!/usr/bin/env python3
+from brain_games.games import progression
+from brain_games.engine import universal_engine
 
 
 def main():
-    progression_game()
+    universal_engine(progression)
+
 
 if __name__ == '__main__':
     main()
