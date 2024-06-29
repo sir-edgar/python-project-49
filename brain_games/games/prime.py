836838b852216e9f5c015ebb7ba58ef81089cfa3
Timur Vitrov <edgar.sir@yandex.ru>
2024-06-29 13:46:32 +0300

    add new func in prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,16 +4,17 @@ from random import randint
 RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
+def is_prime(num):
+    for n in range(2, int(num ** 0.5) + 1):
+        if num % n == 0:
+            return False
+    return True
+
+
 def get_question_and_answer():
     question = randint(1, 100)
-    num = question
-    if num > 1:
-        for n in range(2, num):
-            if (num % n) == 0:
-                correct_answer = 'no'
-                return correct_answer, question
-            correct_answer = 'yes'
+    if is_prime(question):
+        correct_answer = 'yes'
     else:
         correct_answer = 'no'
-        return correct_answer, question
     return correct_answer, question
