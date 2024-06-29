7ce24e68621eb50f409fdb8e1df8178b058fe69f
Timur Vitrov <edgar.sir@yandex.ru>
2024-06-29 13:55:19 +0300

    add new func in progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,19 +4,24 @@ from random import randint
 RULES = "What number is missing in the progression?"
 
 
-def get_question_and_answer():
+def make_progression(a, b):
     i = 0
-    a = randint(1, 50)
-    b = randint(1, 5)
-    int_fot_points = randint(1, 10)
+    index_missing_number = randint(1, 10)
     question = str(a) + ' '
     while i < 10:
         c = (a + b)
         a = c
         i += 1
-        if i != int_fot_points:
+        if i != index_missing_number:
             question += str(c) + ' '
         else:
             correct_answer = c
             question += '.. '
+    return correct_answer, question
+
+
+def get_question_and_answer():
+    first_number = randint(1, 50)
+    second_number = randint(1, 5)
+    correct_answer, question = make_progression(first_number, second_number)
     return correct_answer, question
