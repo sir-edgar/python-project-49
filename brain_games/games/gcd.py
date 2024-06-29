376aefc6cfd746a4c3fecf02ffb78ca48a913c74
Timur Vitrov <edgar.sir@yandex.ru>
2024-06-29 13:39:52 +0300

    add new func in gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -4,18 +4,19 @@ from random import randint
 RULES = "Find the greatest common divisor of given numbers."
 
 
-def get_question_and_answer():
-    a = randint(5, 50)
-    b = randint(5, 50)
-    question = (f'{a} {b}')
-
-    while a != 0 and b != 0:
-
-        if a > b:
-            a = a % b
-
+def is_gcd(num1, num2):
+    question = f'{num1} {num2}'
+    while num1 != 0 and num2 != 0:
+        if num1 > num2:
+            num1 = num1 % num2
         else:
-            b = b % a
+            num2 = num2 % num1
+    correct_answer = num1 + num2
+    return correct_answer, question
 
-    correct_answer = a + b
+
+def get_question_and_answer():
+    first_num = randint(5, 50)
+    second_num = randint(5, 50)
+    correct_answer, question = is_gcd(first_num, second_num)
     return correct_answer, question
